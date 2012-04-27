ff2079675fe094d284b9093ec9b6db55c3a26338
Heidi von Markham <hvonmarkham@mudynamics.com>
2012-04-27 12:40:29 -0700

    reworking main file to redirect to public

--- web.rb
+++ web.rb
@@ -1,15 +1,19 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world. It's me, Heidi."
+  send_file File.join(settings.public_folder, 'index.html')
 end
 
-set :public_filter, File.dirname(__FILE__) + "/public"
+#! get '/' do
+#!  "Hello, world. It's me, Heidi."
+#! end
 
-get '/mu-32993297-ebfb7231-dd9327aa-f8506f05' do
-  '42'
-end
+#! set :public_filter, File.dirname(__FILE__) + "/public"
 
-get '/mu-141b641a-e4005ec9-c484641f-dd66eb4a' do
-  '42'
-end
+#! get '/mu-32993297-ebfb7231-dd9327aa-f8506f05' do
+#!  '42'
+#!end
+
+#!get '/mu-141b641a-e4005ec9-c484641f-dd66eb4a' do
+#!  '42'
+#!end
