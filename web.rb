1b6b82654f55b22e4bee33479fe4c63ed54e7f61
Heidi von Markham <hvonmarkham@mudynamics.com>
2012-04-27 13:06:17 -0700

    uncommenting blitz auth

--- web.rb
+++ web.rb
@@ -10,10 +10,10 @@ end
 
 #! set :public_filter, File.dirname(__FILE__) + "/public"
 
-#! get '/mu-32993297-ebfb7231-dd9327aa-f8506f05' do
-#!  '42'
-#!end
-
 #!get '/mu-141b641a-e4005ec9-c484641f-dd66eb4a' do
 #!  '42'
 #!end
+
+get '/mu-32993297-ebfb7231-dd9327aa-f8506f05' do
+  '42'
+end
