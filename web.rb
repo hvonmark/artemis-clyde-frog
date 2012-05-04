1d1097210f6cfe8f8fb5bbbd7e89f99717fbd0e1
Heidi von Markham <hvonmarkham@mudynamics.com>
2012-05-04 10:16:12 -0700

    adding initial files

---
+++ web.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+get '/' do
+  send_file File.join(settings.public, 'index.html')
+end
+
+#! get '/' do
+#!  "Hello, world. It's me, Heidi."
+#! end
+
+#! set :public_filter, File.dirname(__FILE__) + "/public"
+
+#!get '/mu-141b641a-e4005ec9-c484641f-dd66eb4a' do
+#!  '42'
+#!end
+
+get '/mu-32993297-ebfb7231-dd9327aa-f8506f05' do
+  '42'
+end
